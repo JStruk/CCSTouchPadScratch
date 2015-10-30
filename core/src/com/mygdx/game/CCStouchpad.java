fe8e2790fc6555a0efcac77db5e31c9a303d8144
Justin <strukjustin@gmail.com>
2015-10-30 10:25:57 -0400

    Simple touchpad scratch from yours truly at Cafe CS.

---
+++ core/src/com/mygdx/game/CCStouchpad.java
@@ -0,0 +1,80 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
+public class CCStouchpad extends ApplicationAdapter {
+	private OrthographicCamera camera;
+	Stage stage;
+	private SpriteBatch batch;
+	private Touchpad touchpad;
+	private Touchpad.TouchpadStyle touchpadStyle;
+	private Skin skTouchPad;
+	private Drawable drwTouchbg;
+	private Drawable drwTouchpad;
+	private Texture txRect;
+	private Sprite sprRect;
+	private float fSpeed;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		float aspectRatio = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 10f * aspectRatio, 10f);
+		//Create a skin for the touchpad
+		skTouchPad = new Skin();
+		//Set background and knob imgs
+		skTouchPad.add("drwTouchbg", new Texture("touchBackground.png"));
+		skTouchPad.add("drwTouchpad", new Texture("touchKnob.png"));
+		touchpadStyle = new Touchpad.TouchpadStyle();
+		//make drawables based off the skin
+		drwTouchbg = skTouchPad.getDrawable("drwTouchbg");
+		drwTouchpad = skTouchPad.getDrawable("drwTouchpad");
+		//Apply the bg and knob drawables to the touchpad
+		touchpadStyle.background = drwTouchbg;
+		touchpadStyle.knob = drwTouchpad;
+		touchpad = new Touchpad(10, touchpadStyle);
+		//Initiate the touchpad based on the style we just created
+		touchpad.setBounds(15, 15, 200, 200);
+		//set where the touchpad will be on the screen
+		stage = new Stage();
+		//create the stage and add the touchpad to it
+
+		stage.addActor(touchpad);
+		Gdx.input.setInputProcessor(stage);
+
+		//create our crappy rect that is going to move around with the touchpad
+		txRect = new Texture(Gdx.files.internal("block.png"));
+		sprRect = new Sprite(txRect);
+		sprRect.setPosition(Gdx.graphics.getWidth()/2-sprRect.getWidth()/2, Gdx.graphics.getHeight()/2-sprRect.getHeight()/2);
+		fSpeed = 5;
+		//set the default speed to multiply by when the touchpad is moved around to move the rect
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.294f, 0.294f, 0.294f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+
+		//Move the rect based on the knob percent
+		sprRect.setX(sprRect.getX() + touchpad.getKnobPercentX()*fSpeed);
+		sprRect.setY(sprRect.getY() + touchpad.getKnobPercentY()* fSpeed);
+
+		batch.begin();
+		sprRect.draw(batch);
+		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+	}
+}
