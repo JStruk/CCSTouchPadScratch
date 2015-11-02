3c4264ba2a56c382dc0813ee230832ae5acf3001
Justin <strukjustin@gmail.com>
2015-11-02 13:21:34 -0500

    added our own graphics to the touchpad

--- core/src/com/mygdx/game/CCStouchpad.java
+++ core/src/com/mygdx/game/CCStouchpad.java
@@ -36,9 +36,9 @@ public class CCStouchpad extends ApplicationAdapter {
 		//Create a skin for the touchpad
 		skTouchPad = new Skin();
 		//Set background and knob imgs
-		txFG= new Texture("ThumbstickFG.png");
+		txFG= new Texture("ThumbstickFGsmall.png");
 		sprFG = new Sprite(txFG);
-		sprFG.setBounds(15,15,100,100);
+		sprFG.setBounds(0,0,100,100);
 		skTouchPad.add("drwTouchbg", new Texture("ThumbstickBG.png"));
 		skTouchPad.add("drwTouchpad", sprFG);
 		touchpadStyle = new Touchpad.TouchpadStyle();
@@ -48,10 +48,7 @@ public class CCStouchpad extends ApplicationAdapter {
 		//Apply the bg and knob drawables to the touchpad
 		touchpadStyle.background = drwTouchbg;
 		touchpadStyle.knob = drwTouchpad;
-		touchpadStyle.knob.setTopHeight(10);
-		touchpadStyle.knob.setBottomHeight(10);
-		touchpadStyle.knob.setLeftWidth(10);
-		touchpadStyle.knob.setRightWidth(10);
+
 		touchpad = new Touchpad(10, touchpadStyle);
 		//Initiate the touchpad based on the style we just created
 		touchpad.setBounds(15, 15, 200, 200);
